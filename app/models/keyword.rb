85a0dd1bb54fa82a13b6e75e6309984c6ac91247
Johnny Casual <johnniecasual@gmail.com>
2015-04-02 19:39:48 +0300

    added twitter-gem

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,14 @@
 class Keyword < ActiveRecord::Base
+    def grab_tweets
+      client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "zpFCbeWMwQSHYFvOg3L8ShNce"
+  config.consumer_secret     = "06iNv4EZdRvA6IbYbqRkezZ3ThiAb23kizGlYVpfGQntWAYVwm"
+  config.access_token        = "2876713174-jmjXIDoVNR1D7OsoOj3NMYfWo4Jkas9QFaWuRlr"
+  config.access_token_secret = "UP0gSccYHPsmYQMIOyy5j1ecitI4d7dof5QBq9RxNQw2e"
+end
+
+      client.search(self.word, result_type: "recent").take(3).collect do |tweet|
+  "#{tweet.user.screen_name}: #{tweet.text}"
+      end    
+    end
 end
