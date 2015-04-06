703b3183f638a27dfed2efeeb5d3bac03ca52810
Johnny Casual <johnniecasual@gmail.com>
2015-04-06 17:35:59 +0300

    addede twitter-profilepics

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -18,7 +18,7 @@ end
   new_tweet.user_uid = tweet.user.id
   new_tweet.user_name = tweet.user.name
   new_tweet.user_screen_name = tweet.user.screen_name
-  #new_tweet.user_image_url = tweet.user.profile_image_url
+  new_tweet.user_image_url = tweet.user.profile_image_url.to_s
           
   new_tweet.keyword = self
   
