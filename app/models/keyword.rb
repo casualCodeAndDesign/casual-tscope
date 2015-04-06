7b4cf4735eff7909bd10cd8302982169615d240a
Johnny Casual <johnniecasual@gmail.com>
2015-04-06 18:01:56 +0300

    created task

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,28 +1,33 @@
 class Keyword < ActiveRecord::Base
     has_many :tweets
     def grab_tweets
-      client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "zpFCbeWMwQSHYFvOg3L8ShNce"
-  config.consumer_secret     = "06iNv4EZdRvA6IbYbqRkezZ3ThiAb23kizGlYVpfGQntWAYVwm"
-  config.access_token        = "2876713174-jmjXIDoVNR1D7OsoOj3NMYfWo4Jkas9QFaWuRlr"
-  config.access_token_secret = "UP0gSccYHPsmYQMIOyy5j1ecitI4d7dof5QBq9RxNQw2e"
-end
+        client = Twitter::REST::Client.new do |config|
+            config.consumer_key        = "zpFCbeWMwQSHYFvOg3L8ShNce"
+            config.consumer_secret     = "06iNv4EZdRvA6IbYbqRkezZ3ThiAb23kizGlYVpfGQntWAYVwm"
+            config.access_token        = "2876713174-jmjXIDoVNR1D7OsoOj3NMYfWo4Jkas9QFaWuRlr"
+            config.access_token_secret = "UP0gSccYHPsmYQMIOyy5j1ecitI4d7dof5QBq9RxNQw2e"
+        end
 
-      client.search(self.word, result_type: "recent").take(10).collect do |tweet|
-  new_tweet = Tweet.new
-  
-  new_tweet.tweet_id = tweet.id.to_s
-  new_tweet.tweet_created_at = tweet.created_at
-  new_tweet.text = tweet.text
-  
-  new_tweet.user_uid = tweet.user.id
-  new_tweet.user_name = tweet.user.name
-  new_tweet.user_screen_name = tweet.user.screen_name
-  new_tweet.user_image_url = tweet.user.profile_image_url.to_s
-          
-  new_tweet.keyword = self
-  
-  new_tweet.save
-      end    
+        client.search(self.word, result_type: "recent").take(100).collect do |tweet|
+            new_tweet = Tweet.new
+
+            new_tweet.tweet_id = tweet.id.to_s
+            new_tweet.tweet_created_at = tweet.created_at
+            new_tweet.text = tweet.text
+
+            new_tweet.user_uid = tweet.user.id
+            new_tweet.user_name = tweet.user.name
+            new_tweet.user_screen_name = tweet.user.screen_name
+            new_tweet.user_image_url = tweet.user.profile_image_url.to_s
+
+            new_tweet.keyword = self
+
+            new_tweet.save
+        end    
+    end
+    def self.grab_all_tweets
+        Keyword.all.each do |keyword|
+           keyword.grab_tweets 
+        end
     end
 end
